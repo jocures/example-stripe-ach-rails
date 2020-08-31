ef6f408c9b68016e28a4872744ce0b4665363825
jocures <josephcureton@Josephs-MacBook-Pro.local>
2020-08-30 21:35:13 -0400

    testing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,5 @@
 ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.default_url_options = { host: 'seedcommons.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
